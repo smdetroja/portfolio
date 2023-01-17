7c62c207fe8ac8ad33ecaf6636fe4c80fb985a18
Sanket Detroja <smdetroja710@gmail.com>
2023-01-18 03:18:43 +0530

    main

---
+++ contactform/contactform.php
@@ -0,0 +1,19 @@
+<?php
+		$name = $_POST['name'];
+		$email= $_POST['email'];
+		$subject =  $_POST['subject']
+		$message= $_POST['message'];
+
+		$to = "godkaecilius@gmail.com";
+
+		$txt = "Name = ". $name . "\r\n Email = "
+			. $email . "\r\n Message =" ." . $message;
+
+		$headers .= 'From:'.$email .'\r\n To : @smdetroja.com \r\n";
+			
+		if($email != NULL) {
+			mail($to, $subject, $txt, $headers);
+		}
+
+		header("Location:file: index.html");
+?>
